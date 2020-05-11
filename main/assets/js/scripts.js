dcd4ab0762fc790c324151d1dae1b3093b446ce4
iz-syed <mubarakcc7@gmail.com>
2020-05-11 13:05:40 +0530

    #

--- main/assets/js/scripts.js
+++ main/assets/js/scripts.js
@@ -10691,7 +10691,3 @@ consoleLog("hello-succes", "success");
 consoleLog("hello-dblue", "DodgerBlue");
 consoleLog("hello-error", "error");
 consoleLog("hello-warn", "warning");
-
-
-//homepage main banner - slick slider
-$('.single-item').slick();
