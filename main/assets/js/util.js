7b82ff89051ea37aff6af395012d34ee28e30e99
Sathish <sathish@izaaptech.in>
2020-05-12 18:24:40 +0530

    Page banner

--- main/assets/js/util.js
+++ main/assets/js/util.js
@@ -29,8 +29,13 @@ consoleLog("hello-warn", "warning");
 //$('.single-item').slick();
 
 $(".single-item").slick({
-	dots: true
+	dots: true,
+	arrows: true,
+	prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+	nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
 });
+
+
 
 
 // left sidebar toggle
