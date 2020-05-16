f28d69ca29cbfb6d70bd160715caf1cafb683ae8
Sathish <sathish@izaaptech.in>
2020-05-17 00:33:37 +0530

    script works

--- main/assets/js/util.js
+++ main/assets/js/util.js
@@ -47,7 +47,7 @@ $(".product-detail-slider").slick({
 
 
 $(window).scroll(function () {
-  if ($(window).scrollTop() >= 380) {
+  if ($(window).scrollTop() >= 130) {
     $('header .logo-nav-search').addClass('fixed-header');
     $('.logo .main-logo').addClass('hide-title');
     $('.logo .sub-logo').addClass('visible-title');
@@ -85,3 +85,65 @@ function openSearch() {
 function closeSearch() {
   document.getElementById("searchOverlay").style.display = "none";
 }
+
+
+
+//slideDown, slideUp, slideToggle
+$("#flip").click(function(){
+  $("#panel").slideDown();
+});
+
+$("#flip").click(function(){
+  $("#panel").slideUp();
+});
+
+$("#flip").click(function(){
+  $("#panel").slideToggle();
+});
+
+
+$('#hello').hide('slide', {direction: 'left'}, 1000);
+
+
+//Fade-in-out
+$('#slide-in').click(function(){
+            $('img').fadeIn('fast');
+        });
+        
+$('#fade-out').click(function(){
+            $('img').fadeOut(400);
+        });
+        
+$('#toggle-fading').click(function(){
+            $('img').fadeToggle('slow');
+        });
+
+
+//Product filter icon(sidebar show hide)
+
+var closeIcon = $('<span class="closeicon"><svg viewBox="0 0 32 32"><polyline points="32,3.3 20.9,14.4 19.3,16 20.9,17.6 32,28.7 28.7,32 17.6,20.9 16,19.3 14.4,20.9 3.3,32.1 0,28.8 11.1,17.6 12.7,16 11.1,14.4 0,3.3 3.3,0 14.4,11.1 16,12.7 17.6,11.1 28.7,0 32,3.3 "></polyline></svg><span/>');
+$('.product-sidebar .left-sidebar').prepend(closeIcon);
+
+closeIcon.click(function() {
+//$('.product-sidebar').fadeToggle('slow');
+ }); 
+
+
+$('.productFilter,.closeicon').click(function(){
+            $('.product-sidebar').fadeToggle('slow');
+            //$('.products-listing-grid').fadeIn(3000);
+            
+        });
+
+
+
+
+//Toggle class
+$("parenttag").click(function(){
+        $(this).toggleClass("highlight");
+    });
+
+//Product filter icon active
+$(".productFilter").click(function(){
+        $(this).toggleClass("filter_active");
+    });
